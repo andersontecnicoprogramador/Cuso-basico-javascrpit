4f9813c78cb846b5a469c2f2aa460c6da0ae222a
Anderson Moreira <anderson.tecnicoprogramdor@hotmail.com>
2023-12-28 00:03:38 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// Variaveis
+// Utilize sempre o camel case
+//javascript possui tipagem dinamica:infere os tipos de dados
+//javascript possui uma tipagem fraca
+
+var nomeDoProfessor = 'Anderson Moreira'
+var idade = 41; // number
+var altura = 1.71; // number
+var estudando = true; // boolean (booleano: verdadeiro ou falso)
+
+console.log(nomeDoProfessor,typeof nomeDoProfessor);
+
+
+console.log(idade, typeof idade);
+
+console.log(altura, typeof altura);
+
+console.log(estudando,typeof estudando);
+
+var semConteudo; // declarando uma varialvel
+
+console.log(semConteudo);
+
+var curso = "Front-end em React",
+ topicos = "Javascript Básico I";
+
+ console.log(curso, topicos);
+
+ // ! Não utilizar o var nos seus codigos Javascript
+
+
+
+
+ let notaDoAluno = 10;
+ const mediaDoAluno = 8;
+
+ notaDoAluno = 9;
+ // ! mediaDoAluno = 5; Não é permitido
+
+ console.log(notaDoAluno);
+ console.log(mediaDoAluno);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+ let notaDoAluno = 10;
+ const mediaDoAluno = 8;
+
+ notaDoAluno = 9;
+ // ! mediaDoAluno = 5; Não é permitido
+
+ console.log(notaDoAluno);
+ console.log(mediaDoAluno);
